540d82c19b2c206a1113561d5eb69b2b1f20ee41
Lei Wang <cnkknd@gmail.com>
2022-04-07 18:27:57 +0000

    fix wrong data uri

--- src/lib/Image.svelte
+++ src/lib/Image.svelte
@@ -2,7 +2,7 @@
 	export let alt: string | undefined = undefined;
 	export let src: string | Promise<string> = undefined;
 	export let presrc: string =
-		'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+P+/HgAFhAJ/wlseKgAAAABJRU5ErkJggg==';
+		'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
 	let klass: string | undefined = undefined;
 	export { klass as class };
 </script>
